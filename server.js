9d5faa8f6abd0a94c1aee6778aa0707c3507d37b
layan jafar <layab66@gmail.com>
2026-05-07 00:37:35 +0300

    Set homepage

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 ----------------------------- */
 app.use(express.json());
 app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
 
 /* -----------------------------
    In-memory trip data
